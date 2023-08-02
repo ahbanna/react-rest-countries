cdb65d0e34f46eb961b3567e56437523c87600a4
Al Hasanul Banna <tutul5800@gmail.com>
2023-08-02 17:38:35 +0600

    apply asyan await and axios

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
+import axios from "axios";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
@@ -10,6 +11,25 @@ const Countries = () => {
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+
+  // // USING ASYNC AWAIT
+  // useEffect(() => {
+  //   const loadData = async () => {
+  //     const res = await fetch("https://restcountries.com/v3.1/all");
+  //     const data = await res.json();
+  //     setCountries(data);
+  //   };
+  //   loadData();
+  // }, []);
+
+  // // USING AXIOS
+  // useEffect(() => {
+  //   axios.get("https://restcountries.com/v3.1/all").then((res) => {
+  //     console.log(res);
+  //     setCountries(res.data);
+  //   });
+  // }, []);
+
   return (
     <div className="countries-area">
       <h2>Countries Of The World</h2>
